a71dd44380bec5bee4fafca06bb54725e892cc86
Ανδρεαδάκης Κωνσταντίνος <andreadakis_k@datacomm.gr>
2017-01-16 12:25:45 +0200

    firstcommit

---
+++ HotelManagerLib/Models/Persistant/Services.cs
@@ -0,0 +1,60 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Services.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The services.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace HotelManagerLib.Models.Persistant
+{
+    #region
+
+    using System;
+
+    using HotelManagerLib.Models.Persistant.Interfaces;
+
+    #endregion
+
+    /// <summary>
+    /// The services.
+    /// </summary>
+    class Services : IEntity, IEntityAudit
+    {
+        /// <summary>
+        /// Gets or sets the created.
+        /// </summary>
+        public DateTime Created { get; set; }
+
+        /// <summary>
+        /// Gets or sets the created by.
+        /// </summary>
+        public string CreatedBy { get; set; }
+
+        /// <summary>
+        /// Gets or sets the deleted.
+        /// </summary>
+        public DateTime? Deleted { get; set; }
+
+        /// <summary>
+        /// Gets or sets the deleted by.
+        /// </summary>
+        public string DeletedBy { get; set; }
+
+        /// <summary>
+        /// Gets or sets the id.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the updated.
+        /// </summary>
+        public DateTime? Updated { get; set; }
+
+        /// <summary>
+        /// Gets or sets the updated by.
+        /// </summary>
+        public string UpdatedBy { get; set; }
+    }
+}
