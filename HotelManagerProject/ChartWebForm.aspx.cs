43638a255558388f62618a86d4b4b4d7201f8070
Xaris Megalos <megalos_ch@datacomm.gr>
2017-01-24 12:52:28 +0200

    chart

--- HotelManagerProject/ChartWebForm.aspx.cs
+++ HotelManagerProject/ChartWebForm.aspx.cs
@@ -1,16 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ChartWebForm.aspx.cs" company="Data Communication">
+//   Hotel Manager
+// </copyright>
+// <summary>
+//   The chart web form.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace HotelManagerProject
 {
+    #region
+
+    using System;
+    using System.Web.UI;
+
     using DevExpress.DashboardWeb;
 
-    public partial class ChartWebForm : System.Web.UI.Page
+    using HotelManagerLib.Controllers;
+    using HotelManagerLib.Controllers.Interfaces;
+    using HotelManagerLib.Models.Persistant;
+
+    #endregion
+
+    /// <summary>
+    /// The chart web form.
+    /// </summary>
+    public partial class ChartWebForm : Page
     {
+        /// <summary>
+        /// The billing controller.
+        /// </summary>
+        private IEntityController<Billing> billingController;
+
+        /// <summary>
+        /// The page_ load.
+        /// </summary>
+        /// <param name="sender">
+        /// The sender.
+        /// </param>
+        /// <param name="e">
+        /// The e.
+        /// </param>
         protected void Page_Load(object sender, EventArgs e)
         {
             var dashboard = new Dashboard1();
@@ -18,12 +48,20 @@ namespace HotelManagerProject
             this.ASPxDashboardViewer1.DataLoading += this.AsPxDashboardViewer1OnDataLoading;
         }
 
+        /// <summary>
+        /// The dashboard viewer on data loading.
+        /// </summary>
+        /// <param name="sender">
+        /// The sender.
+        /// </param>
+        /// <param name="e">
+        /// The e.
+        /// </param>
         private void AsPxDashboardViewer1OnDataLoading(object sender, DataLoadingWebEventArgs e)
         {
-            //var
-
-            //var entityDocumentController = new EntityDocumentController();
-            //var documentList = entityDocumentController.RefreshEntities();
+            this.billingController = new BillingEntityController();
+            var billingList = this.billingController.RefreshEntities();
+            e.Data = billingList;
         }
     }
 }
